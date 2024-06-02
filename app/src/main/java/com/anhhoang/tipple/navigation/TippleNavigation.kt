6e1e9c9330f0fa41875a521c91ca6891f1f08919
Anh Hoang <h.anh.viet91@gmail.com>
2024-06-03 02:24:03 +0300

    WIP: Implement search cocktail screen

--- app/src/main/java/com/anhhoang/tipple/navigation/TippleNavigation.kt
+++ app/src/main/java/com/anhhoang/tipple/navigation/TippleNavigation.kt
@@ -1,10 +1,15 @@
 package com.anhhoang.tipple.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.anhhoang.tipple.feature.searchcocktails.SearchCocktailsScreen
+import com.anhhoang.tipple.feature.searchcocktails.SearchCocktailsViewModel
 
 /** Main navigation composable. */
 @Composable
@@ -16,7 +21,9 @@ fun TippleNavigation(
         startDestination = MainScreen
     ) {
         composable<MainScreen> {
-
+            val viewModel = hiltViewModel<SearchCocktailsViewModel>()
+            val state by viewModel.state.collectAsStateWithLifecycle()
+            SearchCocktailsScreen(state, onAction = viewModel::onAction)
         }
         composable<DetailsScreen> {
 
---
+++ feature/searchcocktails/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.google.dagger.hilt)
+}
+
+android {
+    namespace = "com.anhhoang.tipple.feature.searchcocktails"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":core:data"))
+    implementation(libs.kotlinx.coroutines)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.coil)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.dagger.hilt)
+    ksp(libs.dagger.hilt.compiler)
+}
