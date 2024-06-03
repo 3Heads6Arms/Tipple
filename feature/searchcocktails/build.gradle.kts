0ab06aa6a20fb02e268847487c7de1c498fc74bf
Anh Hoang <h.anh.viet91@gmail.com>
2024-06-03 14:18:47 +0300

    Updated ui with basic cocktail item

--- feature/searchcocktails/build.gradle.kts
+++ feature/searchcocktails/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.coil)
+    implementation(libs.coil.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
