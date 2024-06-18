ceaa23414d57bacb03e2c9c4e8ab14dc17229733
Anh Hoang <h.anh.viet91@gmail.com>
2024-06-18 21:21:56 +0300

    Revert "change module to android module"
    This reverts commit 64c05bfb783b465e7d24207fee7b0e1a8a9d3604.

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,43 +1,12 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("java-library")
+    alias(libs.plugins.jetbrains.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
 }
 
-android {
-    namespace = "com.anhhoang.tipple.core.network"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    kotlin {
-        compilerOptions {
-            languageVersion.set(KotlinVersion.KOTLIN_2_0)
-        }
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
