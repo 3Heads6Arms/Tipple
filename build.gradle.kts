3fee9d4ce21bfd12b037d6f61d2303440733c86d
Anh Hoang <h.anh.viet91@gmail.com>
2024-06-18 21:21:56 +0300

    Revert "Add hilt and ksp compiler yaml"
    This reverts commit c6d6d337b38755d8e60b618127ecd228c36d8aac.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,4 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.google.dagger) apply false
-    alias(libs.plugins.google.ksp) apply false
 }
