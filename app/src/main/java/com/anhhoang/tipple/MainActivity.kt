47c2d9d7776d8dd63ec76d4a5de3c0a4d4a50ac0
Anh Hoang <h.anh.viet91@gmail.com>
2024-06-18 21:21:56 +0300

    Revert "Setup Hilt and compose navigation to main App module"
    This reverts commit 6500eec5f35fb255a8f6ac6e46b5dfcda2712489.

--- app/src/main/java/com/anhhoang/tipple/MainActivity.kt
+++ app/src/main/java/com/anhhoang/tipple/MainActivity.kt
@@ -4,19 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.anhhoang.tipple.design.theme.TippleTheme
-import com.anhhoang.tipple.navigation.TippleNavigation
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            TippleTheme {
-                TippleNavigation()
-            }
-        }
+        setContent {}
     }
 }
